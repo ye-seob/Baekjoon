d0c0086c957aaf7c8c3ff84b593179c1a1d21a03
ye-seob <161628841+ye-seob@users.noreply.github.com>
2025-09-05 21:23:05 +0900

    s3.17626

---
+++ Class 3/Four Squares.java
@@ -0,0 +1,23 @@
+ import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int[] dp = new int[n + 1];
+        for(int i = 1; i <= n; i++){
+            dp[i] = Integer.MAX_VALUE;
+        }
+        dp[0] = 0;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+
+        System.out.println(dp[n]);
+     
+    }
+}
